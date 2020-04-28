5438ffda7aa857cea6c92298f5479b3d1bbe94db
ian-1 <ian.walton@portmoor.com>
2020-04-28 20:32:37 +0100

    add window close when keyboard Esc

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -3,14 +3,20 @@ require 'gosu'
 class GameWindow < Gosu::Window
   def initialize(width = 1300, height = 730)
     super width, height
-    self.caption = name
+    self.caption = caption
   end
 
   def update; end
 
   def draw; end
 
-  def name
+  def button_down(id)
+    self.close if id == Gosu::KbEscape
+  end
+
+  def button_up; end
+
+  def caption
     'Nations'
   end
 end
--- spec/game_window_spec.rb
+++ spec/game_window_spec.rb
@@ -1,25 +1,33 @@
 require 'spec_helper'
 
 describe GameWindow do
-  describe '.new' do
-    before { @game = GameWindow.new }
+  before { @game_window = GameWindow.new }
 
+  describe '.new' do
     it 'returns a GameWindow object' do
-      expect(@game).to be_an_instance_of GameWindow
+      expect(@game_window).to be_an_instance_of GameWindow
     end
 
     it 'sets a default width' do
-      expect(@game.width).to eq 1300
+      expect(@game_window.width).to eq 1300
     end
 
     it 'sets a default height' do
-      expect(@game.height).to eq 730
+      expect(@game_window.height).to eq 730
+    end
+  end
+
+  describe '.button_down' do
+    it 'closes window when butten pressed is Esc' do
+      keyboard_Esc = Gosu::KbEscape
+      expect_any_instance_of(GameWindow).to receive(:close)
+      @game_window.button_down(keyboard_Esc)
     end
   end
 
   describe '.caption' do
     it "returns a default name of 'Nations'" do
-      expect(GameWindow.new.caption).to eq 'Nations'
+      expect(@game_window.caption).to eq 'Nations'
     end
   end
 
